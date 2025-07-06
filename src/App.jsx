df7cdb925e33c3ce5b078a56bb527c8d2ddf765f
Ashutosh Lakshakar <ashutoshlakshakar@Ashutoshs-MacBook-Air.local>
2025-07-06 13:03:12 +0530

    install and setup done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import MainBuilder from './Page/MainBuilder';
+
+function App() {
+  
+
+
+  return (
+    <>
+    <h1 className='text-red-500'> Hello Ashutosh </h1>
+     <MainBuilder/>
+    </>
+  );
+}
+
+export default App;
